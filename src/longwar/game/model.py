b3227977f0d816293244f883c8a304d84bb9db8d
Tom <tpepels@gmail.com>
2026-09-24 15:25:07 +0100

    Track Hero use per side and Battle

--- src/longwar/game/model.py
+++ src/longwar/game/model.py
@@ -107,6 +107,7 @@ class GameState:
     schemes: list[list[SchemeState | None]] = field(default_factory=empty_schemes)
     stratagems: list[StratagemState | None] = field(default_factory=empty_stratagems)
     stratagem_used: list[bool] = field(default_factory=lambda: [False, False])
+    hero_used: list[bool] = field(default_factory=lambda: [False, False])
     draw_used: list[bool] = field(default_factory=lambda: [False, False])
     active_player: int = 0
     battle: int = 1
@@ -198,6 +199,7 @@ class GameState:
             schemes=schemes,
             stratagems=stratagems,
             stratagem_used=list(self.stratagem_used),
+            hero_used=list(self.hero_used),
             draw_used=list(self.draw_used),
             active_player=self.active_player,
             battle=self.battle,
@@ -291,6 +293,7 @@ class GameState:
                 target_stratagem.revealed = source_stratagem.revealed
 
         self.stratagem_used[:] = source.stratagem_used
+        self.hero_used[:] = source.hero_used
         self.draw_used[:] = source.draw_used
         self.active_player = source.active_player
         self.battle = source.battle
